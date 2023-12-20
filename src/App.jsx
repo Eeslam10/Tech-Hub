871ecde11dbc9f222dc434e98a8861747ac11127
islam sani <islammuhd8@gmail.com>
2023-12-20 12:15:39 +0100

    deleted and added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing/Landing"
+import Signin from "./pages/Signin/Signin";
+import Signup from "./pages/Signup/Signup";
+
 
 const App = () => {
   return ( 
-    <>     
+    <> 
+   
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<Landing />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup/>} />
       </Routes>
     </BrowserRouter>
     </>
--- src/pages/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-const Button = (props) => {
-    return ( 
-        <button onClick={() => props.action()} className={`${props?.filled &&'bg-backgroundColor text-white'}`}>{props.label}
-        </button>
-     );
-}
- 
-export default Button;
