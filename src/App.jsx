5c8e1872db53705095078a4ecead2513b3b81086
islam sani <islammuhd8@gmail.com>
2023-12-21 10:28:15 +0100

    deleted and added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Landing from "./pages/Landing/Landing"
-import Signin from "./pages/Signin/Signin";
-import Signup from "./pages/Signup/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/landing/Landing";
+import Signin from "./pages/signin/Signin";
+import Signup from "./pages/signup/Signup";
 
-
-const App = () => {
-  return ( 
-    <> 
-   
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup/>} />
-      </Routes>
-    </BrowserRouter>
-    </>
-   );
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/signin" element={<Signin />} />
+					<Route path="/signup" element={<Signup />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
- 
+
 export default App;
